df55ba194788a971704068905b708f3b65a803b5
Bárbaro Javier Valmaseda <bjvalmaseda.g@gmail.com>
2023-01-09 15:58:32 +0400

    chore: add some config changes

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,12 +3,27 @@ module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
+      colors: {
+        principal: {
+          100: '#cd550034',
+          200: '#cd550051',
+          300: '#cd550068',
+          400: '#cd550085',
+          500: '#cd5500a2',
+          600: '#cd5500b9',
+          700: '#cd5500c6',
+          800: '#cd5500d3',
+          900: '#cd5500ff',
+          DEFAULT: '#cd5500'
+        },
+        'principal-dark': '#ff6900'
+      },
       fontSize: {
         xxs: '.625rem'
       },
       fontFamily: {
         body: ['Archivo', 'system-ui', 'sans-serif'],
-        black: ['Archivo Black', 'system-ui', 'sans-serif']
+        title: ['Archivo Black', 'system-ui', 'sans-serif']
       }
     }
   },
