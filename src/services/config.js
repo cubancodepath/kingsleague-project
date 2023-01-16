c76bfbaecb072c97838541a7525a282d86fde5c8
Bárbaro Javier Valmaseda <bjvalmaseda.g@gmail.com>
2023-01-16 10:35:25 +0400

    fix: fix api url for production mode

--- src/services/config.js
+++ src/services/config.js
@@ -1,4 +1,4 @@
-export const isDevelopment = import.meta.NODE !== 'production'
+export const isDevelopment = import.meta.env.MODE !== 'production'
 
 export const apiUrl = isDevelopment
   ? 'http://127.0.0.1:8787'
